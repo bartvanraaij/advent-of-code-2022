c0655d077eec798d06ef77adf5162a03aba1bab4
Bart van Raaij <bart.vanraaij@coolblue.nl>
2022-12-08 16:24:20 +0100

    Improve code

--- 08/trees.js
+++ 08/trees.js
@@ -15,113 +15,7 @@ const rows = rawData.split("\n");
 const highestX = rows[0].length -1;
 const highestY = rows.length -1;
 
-const isEdge = (x, y) => {
-  return x===0 || x===highestX || y===0||y===highestY;
-}
-
-
-const allTreesToTheLeftAreLower = (x,y, thisTree) => {
-  return compareTreesInRow(0, x-1, y, thisTree);
-}
-const allTreesToTheRightAreLower = (x,y, thisTree) => {
-  return compareTreesInRow(x+1, highestX, y, thisTree);
-}
-
-const allTreesAboveAreLower = (x,y, thisTree) => {
-  return compareTreesInColumn(0, y-1, x, thisTree);
-}
-
-const allTreesBelowAreLower = (x,y, thisTree) => {
-  return compareTreesInColumn(y+1, highestY, x, thisTree);
-}
-
-const compareTreesInRow = (startX, endX, y, compareWith) => {
-  return compareTreesAmongAxis('x', startX, endX, y, compareWith);
-}
-
-const compareTreesInColumn = (startY, endY, x, compareWith) => {
-  return compareTreesAmongAxis('y', startY, endY, x, compareWith);
-}
-
-const compareTreesAmongAxis = (axis, start, end, otherAxisValue, compareWith) => {
-  for(let i = start; i<=end; i++) {
-    const compareTree = axis === 'x' ? getTree(i,otherAxisValue) : getTree(otherAxisValue, i);
-    if(compareTree >= compareWith) return false;
-  }
-  return true;
-}
-
-const numVisibleTreesToTheLeft = (x,y, thisTree) => {
-  let count = 0;
-  let compareTree;
-  let i = x;
-  if(x===0) return 0;
-  do {
-    i--;
-    count++;
-    compareTree = getTree(i, y);
-  } while(compareTree < thisTree && i > 0);
-  return count;
-}
-const numVisibleTreesToTheRight = (x,y, thisTree) => {
-  let count = 0;
-  let compareTree;
-  let i = x;
-  if(x === highestX) return 0;
-  do {
-    i++;
-    count++;
-    compareTree = getTree(i, y);
-  } while(compareTree < thisTree && i<highestX);
-  return count;
-}
-
-const numVisibleTreesToTheTop = (x,y, thisTree) => {
-  let count = 0;
-  let compareTree;
-  let j = y;
-  if(y===0) return 0;
-  do {
-    j--;
-    count++;
-    compareTree = getTree(x, j);
-  } while(compareTree < thisTree && j > 0);
-  return count;
-}
-const numVisibleTreesToTheBottom = (x,y, thisTree) => {
-  let count = 0;
-  let compareTree;
-  let j = y;
-  if(y === highestY) return 0;
-  do {
-    j++;
-    count++;
-    compareTree = getTree(x, j);
-  } while(compareTree < thisTree && j<highestY);
-  return count;
-}
-
-
-
-
-const isVisible = (x,y, treeHeight) => {
-  if(isEdge(x,y)) {
-    return true;
-  }
-
-  if(allTreesAboveAreLower(x,y, treeHeight) ||
-    allTreesBelowAreLower(x,y, treeHeight) ||
-    allTreesToTheRightAreLower(x,y, treeHeight) ||
-    allTreesToTheLeftAreLower(x,y, treeHeight)
-  ) {
-    return true;
-  }
-
-
-  return false;
-}
-
-
+// Setup forest
 for(let y = 0; y < rows.length; y++) {
   const row = rows[y];
   for(let x = 0; x < row.length; x++) {
@@ -131,6 +25,36 @@ for(let y = 0; y < rows.length; y++) {
 }
 
 // Part 1
+const isEdge = (x, y) => {
+  return x===0 || x===highestX || y===0||y===highestY;
+}
+
+const compareTreesInRow = (startX, endX, y, compareWith) => {
+  for(let i = startX; i<=endX; i++) {
+    const compareTree = getTree(i,y);
+    if(compareTree >= compareWith) return false;
+  }
+  return true;
+}
+
+const compareTreesInColumn = (startY, endY, x, compareWith) => {
+  for(let j = startY; j<=endY; j++) {
+    const compareTree = getTree(x,j);
+    if(compareTree >= compareWith) return false;
+  }
+  return true;
+}
+
+const isVisible = (x,y, treeHeight) => {
+  return (
+    isEdge(x,y) ||
+    compareTreesInColumn(y+1, highestY, x, treeHeight) ||
+    compareTreesInColumn(0, y-1, x, treeHeight) ||
+    compareTreesInRow(0, x-1, y, treeHeight) ||
+    compareTreesInRow(x+1, highestX, y, treeHeight)
+  );
+}
+
 let visibleTreeCount = 0;
 for(let [coord, tree] of forest.entries()) {
   const [x,y] = coordToXY(coord);
@@ -142,13 +66,36 @@ for(let [coord, tree] of forest.entries()) {
 console.log(visibleTreeCount);
 
 // Part 2
+const numVisibleTreesDownward = (x, y, vertical, compareWith) => {
+  let count = 0, compareTree, i = vertical ? y : x;
+  if(i===0) return 0;
+  do {
+    i--;
+    count++;
+    compareTree = vertical ? getTree(x, i) : getTree(i, y);
+  } while(compareTree < compareWith && i > 0);
+  return count;
+}
+
+const numVisibleTreesUpward = (x, y, vertical, compareWith) => {
+  let count = 0, compareTree, i = vertical ? y : x;
+  const highest = vertical ? highestY : highestX;
+  if(i===highest) return 0;
+  do {
+    i++;
+    count++;
+    compareTree = vertical ? getTree(x, i) : getTree(i, y);
+  } while(compareTree < compareWith && i < highest);
+  return count;
+}
+
 let highestScore = 0;
 for(let [coord, tree] of forest.entries()) {
   const [x,y] = coordToXY(coord);
-  const l = numVisibleTreesToTheLeft(x,y,tree);
-  const r = numVisibleTreesToTheRight(x,y,tree);
-  const t = numVisibleTreesToTheTop(x,y,tree);
-  const b = numVisibleTreesToTheBottom(x,y,tree);
+  const l = numVisibleTreesDownward(x,y, false, tree);
+  const r = numVisibleTreesUpward(x,y, false, tree);
+  const t = numVisibleTreesDownward(x,y, true, tree);
+  const b = numVisibleTreesUpward(x,y,true, tree);
   const score = l*r*t*b;
   if(score > highestScore) highestScore = score;
 }
