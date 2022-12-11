d96dbe6915f9e6b03757894ed96c790fcb3d4b96
Bart van Raaij <bart.vanraaij@coolblue.nl>
2022-12-11 19:32:52 +0100

    Day 11: Combine part 1 and 2

--- 11/monkey.js
+++ 11/monkey.js
@@ -6,7 +6,7 @@ String.prototype.toInt = function () {
 }
 const toInt = (str) => parseInt(str, 10);
 
-const monkeys = rawData.split("\n\n").map(str => {
+const getMonkeys = () => rawData.split("\n\n").map(str => {
   const lines = str.split("\n");
   const monkey = lines[0].charAt(7).toInt();
   const items = lines[1].substring(18).split(", ").map(toInt);
@@ -20,72 +20,47 @@ const monkeys = rawData.split("\n\n").map(str => {
 });
 
 // Part 1
-// let numRounds = 20;
-// for(let i=1; i <= numRounds; i++) {
-//
-//   for(let monkey of monkeys) {
-//     for(let item of monkey.items) {
-//       let level;
-//       let operationValue = monkey.operationValue === 'old' ? item : monkey.operationValue.toInt();
-//       if(monkey.operationType === '*') level = item * operationValue;
-//       else level = item + operationValue;
-//       level = Math.floor(level/3);
-//       const destMonkey = (level % monkey.testModulo === 0) ? monkeys[monkey.destIfTrue] : monkeys[monkey.destIfFalse];
-//       destMonkey.items.push(level);
-//       monkey.inspectionCount++;
-//     }
-//     monkey.items = [];
-//   }
-// }
-// for(let monkey of monkeys) {
-//   console.log(`Monkey ${monkey.monkey}: ${monkey.items.join(', ')}`);
-// }
-// for(let monkey of monkeys) {
-//   console.log(`Monkey ${monkey.monkey} inspected items : ${monkey.inspectionCount} times`);
-// }
-// let monkeyActivityScores = monkeys.map(m => m.inspectionCount).sort((a, b) => {
-//   return a - b;
-// }).reverse();
-//
-// const monkeyBusinessLevel = monkeyActivityScores[0] * monkeyActivityScores[1];
-// console.log(monkeyBusinessLevel);
+const getMonkeyBusinessLevel = (monkeys, numRounds, worryLevelSuppressorFn) => {
+  for(let i=1; i <= numRounds; i++) {
+    for(let monkey of monkeys) {
+      for(let item of monkey.items) {
+        let level;
+        if(monkey.operationValue === 'old') {
+          level = item * item;
+        } else {
+          if(monkey.operationType === '*') level = item * monkey.operationValue.toInt();
+          else level = item + monkey.operationValue.toInt();
+        }
+
+        level = worryLevelSuppressorFn(level);
+        const destMonkey = (level % monkey.testModulo === 0) ? monkeys[monkey.destIfTrue] : monkeys[monkey.destIfFalse];
+        destMonkey.items.push(level);
+        monkey.inspectionCount++;
+      }
+      monkey.items = [];
+    }
+  }
+
+  let monkeyActivityScores = monkeys.map(m => m.inspectionCount).sort((a, b) => {
+    return a - b;
+  }).reverse();
+
+  return monkeyActivityScores[0] * monkeyActivityScores[1];
+}
+
+// Part 1
+const monkeyBusinessLevelPart1 = getMonkeyBusinessLevel(getMonkeys(),20,(level) => {
+  return Math.floor(level/3);
+});
+console.log(monkeyBusinessLevelPart1);
 
 // Part 2
-let numRounds = 10000;
+const monkeys = getMonkeys();
 let worryLevelDivider = monkeys.reduce((acc, monkey) => {
   return acc * monkey.testModulo
 }, 1);
 
-for(let i=1; i <= numRounds; i++) {
-
-  for(let monkey of monkeys) {
-    for(let item of monkey.items) {
-      let level;
-      if(monkey.operationValue === 'old') {
-        level = item * item;
-      } else {
-        if(monkey.operationType === '*') level = item * monkey.operationValue.toInt();
-        else level = item + monkey.operationValue.toInt();
-      }
-      level = level % worryLevelDivider;
-
-      const destMonkey = ((level % monkey.testModulo) === 0) ? monkeys[monkey.destIfTrue] : monkeys[monkey.destIfFalse];
-      destMonkey.items.push(level);
-      monkey.inspectionCount++;
-    }
-    monkey.items = [];
-  }
-}
-for(let monkey of monkeys) {
-  console.log(`Monkey ${monkey.monkey}: ${monkey.items.join(', ')}`);
-}
-for(let monkey of monkeys) {
-  console.log(`Monkey ${monkey.monkey} inspected items : ${monkey.inspectionCount} times`);
-}
-let monkeyActivityScores = monkeys.map(m => m.inspectionCount).sort((a, b) => {
-  return a - b;
-}).reverse();
-console.log(monkeyActivityScores);
-
-const monkeyBusinessLevel = monkeyActivityScores[0] * monkeyActivityScores[1];
-console.log(monkeyBusinessLevel);
+const monkeyBusinessLevelPart2 = getMonkeyBusinessLevel(monkeys, 10000, (level) => {
+  return level % worryLevelDivider;
+});
+console.log(monkeyBusinessLevelPart2);
