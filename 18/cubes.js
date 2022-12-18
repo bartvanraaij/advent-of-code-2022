1006b77f796cf3574816bb1bca01f8872084611d
Bart van Raaij <bart.vanraaij@coolblue.nl>
2022-12-18 20:25:05 +0100

    Day 18: Boiling Boulders (part 2)

--- 18/cubes.js
+++ 18/cubes.js
@@ -39,8 +39,122 @@ const getAmountOfFreeSides = ([x,y,z], cubes) => {
 
 
 // Part 1
-const cubes = getCubes(inputData);
-const allFreeSides = cubes.reduce((acc, cube) => {
-  return getAmountOfFreeSides(cube, cubes) + acc;
-}, 0);
-console.log(allFreeSides);
+const countAllFreeSides = (inputData) => {
+  const cubes = getCubes(inputData);
+  return cubes.reduce((acc, cube) => {
+    return getAmountOfFreeSides(cube, cubes) + acc;
+  }, 0);
+}
+const surfaceArea = countAllFreeSides(inputData);
+console.log(surfaceArea);
+
+// Part 2
+// Find the grid size
+const getGridSize = (cubes) => {
+  let minX = Infinity, maxX = -Infinity, minY = Infinity, maxY = -Infinity, minZ = Infinity, maxZ = -Infinity;
+  for(let [x,y,z] of cubes) {
+    if(x < minX) minX = x;
+    if(x > maxX) maxX = x;
+    if(y < minY) minY = y;
+    if(y > maxY) maxY = y;
+    if(z < minZ) minZ = z;
+    if(z > maxZ) maxZ = z;
+  }
+  return {minX,maxX,minY,maxY,minZ,maxZ};
+}
+
+const xyzKey = ([x,y,z]) =>  `${x}.${y}.${z}`;
+
+const build3dGrid = (cubes) => {
+  let grid = [];
+  const emptySpots = [];
+  const gridSize = getGridSize(cubes);
+  const cubeStrings = new Set(cubes.map(xyzKey));
+  for(let i=0; i<=gridSize.maxX; i++) {
+    const thisX = [];
+    for(let j=0; j<=gridSize.maxY; j++) {
+      const thisY = [];
+      for(let k=0; k<=gridSize.maxZ; k++) {
+        if(cubeStrings.has(xyzKey([i,j,k]))) {
+          thisY.push('C')
+        } else {
+          thisY.push('E');
+          emptySpots.push([i,j,k]);
+        }
+      }
+      thisX.push(thisY);
+    }
+    grid.push(thisX);
+  }
+  return {gridSize,grid, emptySpots};
+}
+
+const directions = ['left','right','top','bottom','front','back'];
+const getAdjacentCoords = ([x,y,z], dir) => {
+  if(dir === 'left') return [x-1,y,z];
+  if(dir === 'right') return [x+1, y,z];
+  if(dir === 'top') return [x,y+1,z];
+  if(dir === 'bottom') return [x,y-1,z];
+  if(dir === 'front') return[x,y,z-1];
+  if(dir === 'back') return [x,y,z+1];
+};
+
+const isOnGridEdge = ([x,y,z], gridSize) => {
+  return (x >= gridSize.maxX || x <= gridSize.minX ||
+    y >= gridSize.maxY || y <= gridSize.minY ||
+    z >= gridSize.maxZ ||z <= gridSize.minZ);
+}
+
+const isCube = ([x,y,z], grid) => {
+  return grid[x][y][z] === 'C';
+}
+
+const hasAWayOut = (position, grid, gridSize, visited = [], trapped = []) => {
+
+  let thisPos = xyzKey(position);
+
+  if(trapped.includes(thisPos)) {
+    return false;
+  }
+
+  if(visited.includes(thisPos)) {
+    return false;
+  } // Loop!
+  visited.push(thisPos);
+
+  if(isOnGridEdge(position, gridSize)) {
+    let thisIsBlocked = isCube(position, grid);
+    if(thisIsBlocked) trapped.push(thisIsBlocked);
+    return ! thisIsBlocked;
+  }
+  for(let direction of directions) {
+    let look = getAdjacentCoords(position, direction);
+
+    if(isCube(look, grid)) {
+      visited.push(xyzKey(look));
+      continue;
+    }
+
+    let thisLookWayIsAWayOut = hasAWayOut(look, grid, gridSize, [...visited], trapped);
+    if(thisLookWayIsAWayOut) {
+      return true;
+    }
+  }
+
+  trapped.push(thisPos);
+  return false;
+}
+
+const countAllFreeSidesWithTrappedSpotsAdded = (inputData) => {
+  const cubes = getCubes(inputData);
+  const {gridSize,grid, emptySpots} = build3dGrid(cubes);
+  const trappedEmptySpots = emptySpots.filter(s => ! hasAWayOut(s, grid, gridSize));
+
+  return cubes.reduce((acc, cube) => {
+    return getAmountOfFreeSides(cube, [...cubes,...trappedEmptySpots]) + acc;
+  }, 0);
+}
+
+// Part 2
+const exteriorSurfaceArea = countAllFreeSidesWithTrappedSpotsAdded(inputData);
+console.log(exteriorSurfaceArea);
